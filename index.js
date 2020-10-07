c293cae1b884c9bef9ab8336d8886fd8d8bcb90c
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-10-07 15:15:16 +0700

    Update comments and README

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Directory:
-// folder1
-// --folder2
-// ----somefile1.js
-// ----somefile2.js
-// --folder3
-// ----somefile3.js
-// ----somefile4.json
-// Goal:
-// Achieve an object like so :
-// {
-//   folder2: {
-//     somefile1: {
-//       somefunction: [Function aFunction] // You can call this function if you require it
-//     },
-//     somefile2: {},
-//   },
-//   folder3: {
-//     somefile3: {},
-//     somefile4: {
-//        something: "Something in the json file"
-//     },
-//   }
-// }
-
 var fs = require('fs'), path = require('path')
   
 async function getFilesAsync(path = "./") {
